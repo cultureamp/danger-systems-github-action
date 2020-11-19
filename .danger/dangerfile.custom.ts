da717bf8ebace9784cf2ff17d61b5eeba394cf01
Max Wheeler <max@makenosound.com>
2020-11-20 10:59:45 +1100

    Implement custom path override

---
+++ .danger/dangerfile.custom.ts
@@ -0,0 +1,4 @@
+import { message, warn } from "danger"
+
+message("Message from custom dangerfile")
+warn("Message from custom dangerfile")
--- .danger/dangerfile.ts
+++
@@ -1,4 +0,0 @@
-import { message, warn } from "danger"
-
-message("Message from danger-js")
-warn("Message from danger-js")
